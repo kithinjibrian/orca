bab140dc108b6a962fa9baa719fb975fc0b45b20
kithinjibrian <kithinjibrian369@gmail.com>
2026-01-28 17:33:15 +0300

    version increase

--- packages/pod/src/index.ts
+++ packages/pod/src/index.ts
@@ -32,7 +32,7 @@ import { compileFiles } from "./compile";
 
 const program = new Command();
 
-program.name("pod").description("Pod cli tool").version("1.0.49");
+program.name("pod").description("Pod cli tool").version("1.0.50");
 
 program
   .command("new <name> [type]")
